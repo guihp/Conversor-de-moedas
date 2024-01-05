7f0c0e037cfaf28042973ef6bf408d90296cd25f
Guilherme Barros <g.henrique.barros21@gmail.com>
2024-01-05 00:05:13 -0300

    first commit

---
+++ src/Picker/index.js
@@ -0,0 +1,24 @@
+import React from "react"
+import { Picker } from "@react-native-picker/picker";
+
+
+
+export function PickerItem(props) {
+
+    let moedasItem = props.moedas.map( (item, index) => {
+        return (
+            <Picker.Item value={item.key} key={index} label={item.key} />
+        )
+    })
+
+
+    return(
+        <Picker 
+            selectedValue={props.moedaSelecionada}
+            onValueChange={ (valor) => props.onChange(valor)}
+        >
+            {moedasItem}
+        </Picker>
+
+    )
+}
